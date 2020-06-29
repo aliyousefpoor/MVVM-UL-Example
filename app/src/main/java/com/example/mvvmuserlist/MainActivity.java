9a7330e6dfb93d5aac009e4499f8bd85799a58c6
Ali Yousefpoor <Ali@Mahdis-MacBook-Pro.local>
2020-06-30 03:16:11 +0430

    Add FragmentManager in MainActivity

--- app/src/main/java/com/example/mvvmuserlist/MainActivity.java
+++ app/src/main/java/com/example/mvvmuserlist/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.mvvmuserlist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    UserListFragment userListFragment = new UserListFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.framelayout, userListFragment).addToBackStack(null);
+        transaction.commit();
 
 
     }
